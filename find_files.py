da64bd42aa894511b2be2270777d9d87f2d2d2ec
Mario Burbano <burbano.carmona@gmail.com>
2017-09-18 10:38:42 +0200

    Added find_files.py. An utility to, eh, find files

---
+++ find_files.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import os
+import sys
+import argparse
+import fnmatch
+
+def read_args():
+	parser = argparse.ArgumentParser( description = "find files in directory structure" ) 
+        parser.add_argument( "--path", "-p", metavar = "STR", type=str, required = True, help="The path where to look for the file")
+        parser.add_argument( "--filename", "-f", metavar = "STR", type=str, required = True, help="The filename to look for")
+	return parser.parse_args()
+
+def find_files(path,target):
+	matches = []
+	for root, subFolders, files in os.walk(path):
+		if target in files:
+			matches.append(root)
+	return matches
+
+def find_dirs_files_pattern(path,pattern):
+	matches = []
+	for root, dirnames, filenames in os.walk(path):
+		for filename in fnmatch.filter(filenames, pattern):
+			matches.append([root,filename])
+	return matches
+
+if __name__ == '__main__':
+
+  args = read_args()
+  path = args.path
+  target = args.filename
+  my_files = find_files(path,target)
